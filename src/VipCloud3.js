2502d6559a8eda25ffad013abc463c604fcda215
keating <mgsong@madup.com>
2021-10-04 21:24:34 +0900

    차례대로 20개씩 확대

--- src/VipCloud3.js
+++ src/VipCloud3.js
@@ -1,15 +1,42 @@
 import getDefaultColors from './utils/getDefaultColors'
+import {useEffect, useState} from 'react'
+
+let start = 0
+
+const COUNT = 20
+const MAX = 40
+const MIN = 20
+
 
 export default function VipCloud3({ list }) {
   const colors = getDefaultColors()
+  const vipList = list
+  const [words, setWords] = useState(() => vipList.map(
+      (name, idx) => ({
+        text: name,
+        value: idx < COUNT ? MAX : MIN
+      })))
+
+  useEffect(() => {
+    setTimeout(() => {
+      setWords(list => {
+        start = start + COUNT
+        if(start > vipList.length){
+          start = 0
+        }
+        return list.map((item, idx) => ({text: item.text, value: start <= idx && idx < (start + COUNT) ? MAX : MIN}))
+      })
+    }, 5000)
+  }, [words, vipList.length])
+
   return (
     <div style={{ display: 'flex', padding: '20px 10px 10px', flexWrap: 'wrap', flex: 1 }}>
-      {list.map((name, idx) => (
+      {words.map(({text, value}, idx) => (
         <div
-          style={{ fontSize: 20, padding: '0 10px', color: colors[Math.floor(Math.random() * 20)] }}
+          style={{ fontSize: value, padding: '0 10px', color: colors[Math.floor(Math.random() * 20)] }}
           key={idx}
         >
-          {name}
+          {text}
         </div>
       ))}
     </div>
