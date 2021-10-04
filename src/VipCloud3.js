206d91d09ff4ed5c423cddfc33b2d8d76b9695ce
keating <mgsong@madup.com>
2021-10-04 21:43:15 +0900

    랜덤 20개씩 확대

--- src/VipCloud3.js
+++ src/VipCloud3.js
@@ -1,5 +1,5 @@
 import getDefaultColors from './utils/getDefaultColors'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
 let start = 0
 
@@ -7,33 +7,42 @@ const COUNT = 20
 const MAX = 40
 const MIN = 20
 
-
 export default function VipCloud3({ list }) {
   const colors = getDefaultColors()
   const vipList = list
-  const [words, setWords] = useState(() => vipList.map(
-      (name, idx) => ({
-        text: name,
-        value: idx < COUNT ? MAX : MIN
-      })))
+  const [words, setWords] = useState(() =>
+    vipList.map((name, idx) => ({
+      text: name,
+      value: MIN,
+    })),
+  )
 
   useEffect(() => {
     setTimeout(() => {
       setWords(list => {
-        start = start + COUNT
-        if(start > vipList.length){
+        start = start + 1
+        if (start * COUNT > vipList.length) {
           start = 0
         }
-        return list.map((item, idx) => ({text: item.text, value: start <= idx && idx < (start + COUNT) ? MAX : MIN}))
+        return list.map((item, idx) => ({
+          text: item.text,
+          value: idx % COUNT === Math.floor(Math.random() * COUNT) ? MAX : MIN,
+        }))
       })
     }, 5000)
   }, [words, vipList.length])
 
   return (
     <div style={{ display: 'flex', padding: '20px 10px 10px', flexWrap: 'wrap', flex: 1 }}>
-      {words.map(({text, value}, idx) => (
+      {words.map(({ text, value }, idx) => (
         <div
-          style={{ fontSize: value, padding: '0 10px', color: colors[Math.floor(Math.random() * 20)] }}
+          style={{
+            fontSize: 18,
+            transition: `transform 1s ease-in-out`,
+            transform: value > 20 ? 'scale(2.0)' : 'scale(1.0)',
+            padding: '0 20px',
+            color: colors[Math.floor(Math.random() * 20)],
+          }}
           key={idx}
         >
           {text}
